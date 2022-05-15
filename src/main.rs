8d79c2f58164b0e55d5ecd11dcc97bc184e32ca2
Axel Brinck <axel.louis.1991@gmail.com>
2022-05-15 11:59:58 +0200

    getting user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+enum Command {
+    Add(String),
+    Finish(String),
+    Remove(String)
 }
+
+fn process_command(command: String) {
+    
+}
+
+fn main() {
+    println!("To-Do App made with Rust!");
+    
+    let stdin = std::io::stdin();
+
+    loop {
+        let mut command_raw = String::new();
+
+        let read_result = stdin.read_line(&mut command_raw)
+
+        if let Err(_i) = read_result {
+            println!("An error occured while trying to read from stdin.");
+        }
+
+        let command = command_raw.replace("\r\n", "");
+
+        if command == String::from("exit") {
+            break;
+        }
+
+        process_command(command);
+    }
+}
+
