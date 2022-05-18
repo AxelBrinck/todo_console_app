d91544d4096eead159836e2f7b0991f28b3f8910
Axel Brinck <axel.louis.1991@gmail.com>
2022-05-18 21:12:41 +0200

    adding, removing, displaying tasks

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,120 @@
 enum Command {
     Add(String),
-    Finish(String),
-    Remove(String)
+    Remove(String),
+    Display
 }
 
-// TODO: Evaluate commands that has no arguments.
-// TODO: Implement display command.
-fn deserialize_command(serialized_command: String) -> Option<Command> {
-    let mut split = serialized_command.split_whitespace();
-    let command = split.next();
-    let argument = split.next();
+fn load_tasks() -> Vec<String> {
+    let file = std::fs::read_to_string("tasks.txt");
+    
+    if let Ok(file) = file {
+        let tasks_file = file.split("\r\n");
+        let mut tasks: Vec<String> = Vec::new();
 
-    if let (Some(command), Some(argument)) = (command, argument) {
-        let command = command.to_lowercase();
-        let argument = argument.to_lowercase();
-
-        if command == "add" {
-            return Some(Command::Add(argument))
+        for task in tasks_file {
+            if task != "" {
+                tasks.push(task.to_string());
+            }
         }
 
-        if command == "remove" {
-            return Some(Command::Remove(argument))
-        }
+        return tasks;
+    }
 
-        if command == "finish" {
-            return Some(Command::Finish(argument))
-        }
-        
-        None
-    } else {
-        None
+    Vec::new()
+}
+
+fn save_tasks(tasks: &Vec<String>) {
+    let mut file_contents = String::new();
+
+    for task in tasks {
+        file_contents.push_str(&(task.to_string() + "\r\n"));
+    }
+
+    let write_result = std::fs::write("tasks.txt", file_contents);
+
+    if let Err(_i) = write_result {
+        panic!("Could not write to file.");
     }
 }
 
-// TODO: Handle command actions.
-fn process_command(command: String) {
+fn add_task(task: &str) {
+    let mut tasks = load_tasks();
+
+    tasks.push(task.to_string());
+
+    save_tasks(&tasks);
+}
+
+fn remove_task(task: &str) -> bool {
+    let mut tasks = load_tasks();
+
+    let index = tasks.iter().position(|x| x.to_lowercase() == task.to_lowercase());
+
+    if let Some(x) = index {
+        tasks.remove(x);
+
+        save_tasks(&tasks);
+
+        return true;
+    }
+
+    return false;
+}
+
+fn display_tasks() {
+    let tasks = load_tasks();
+
+    if tasks.len() == 0 {
+        print!("No elements.");
+        return;
+    }
+
+    for task in tasks {
+        println!("{}", task);
+    }
+}
+
+fn deserialize_command(serialized_command: &str) -> Option<Command> {
+
+    let serialized_command = serialized_command.trim();
+
+    if let Some(command) = serialized_command.split_whitespace().next() {
+        let command = command.trim().to_lowercase();
+        let argument = &serialized_command[command.chars().count()..].trim();
+
+        if command == "add" {
+            return Some(Command::Add(argument.to_string()));
+        }
+
+        if command == "remove" {
+            return Some(Command::Remove(argument.to_string()))
+        }
+
+        if command == "display" {
+            return Some(Command::Display)
+        }
+    }
+
+    None
+}
+
+fn process_command(command: &str) {
     let command = deserialize_command(command);
 
-    if let None = command {
+    if let Some(command) = command {
+        match command {
+            Command::Add(task) => add_task(&task),
+            Command::Remove(task) => {
+                if remove_task(&task) {
+                    println!("OK!");
+                } else {
+                    println!("Task not found.");
+                }
+            },
+            Command::Display => display_tasks()
+        }
+    } else {
         println!("Unrecognized command.");
-        return;
     }
 }
 
@@ -59,7 +134,7 @@ fn main() {
             break;
         }
 
-        process_command(command);
+        process_command(&command);
     }
 }
 
